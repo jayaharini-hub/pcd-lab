e804e8f269900a5857f3a75712c4203e93b260be
jayaharini-hub <125259320+jayaharini-hub@users.noreply.github.com>
2025-06-29 19:35:38 +0530

    Delete week1_data directory

--- week1_data/Week1_Design_Patterns_and_Principles/Exercise11_DependencyInjectionExample/DITest.java
+++
@@ -1,7 +0,0 @@
-public class DITest {
-    public static void main(String[] args) {
-        CustomerRepository repository = new CustomerRepositoryImpl();
-        CustomerService service = new CustomerService(repository);
-        service.getCustomerDetails(101);
-    }
-}
